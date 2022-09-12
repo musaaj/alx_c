8d5ffdfed2f6c2f60316d139d472ddd57939c292
Musa Ibrahim <=>
2022-09-12 10:44:58 +0100

    Auto Commit

---
+++ char.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char a[] = "ba";
+	char b[] = "ac";
+	char d = 102;
+
+	printf("%c\n", d);
+	if (a > b)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+}
---
+++ float.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	float f = 0.1;
+
+	if( f == 0.1f)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+
+	return 0;
+}
